6b6e2f8efa4e8a3704759eb7d4eb37832a18d9bc
sven <sven@so36.net>
2018-07-18 12:53:52 +0200

    ci: Update deploy script

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-zip -r website.zip ~/build/html/site
+zip -r website.zip ~/build/html/build/site/
 
 #curl -H "Content-Type: application/zip" \
 #     -H "Authorization: Bearer $NETLIFYKEY" \
